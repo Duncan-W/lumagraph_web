3f60038e2a44f2a4a86adf9bfba8fbb08dd44ff9
unknown <duncan.d.wallace@gmail.com>
2024-08-04 10:54:49 +0100

    nav dismiss button

---
+++ resources/js/menu.js
@@ -0,0 +1,33 @@
+// Show button when scrolled down the page
+window.addEventListener('scroll', function() {
+    const button = document.getElementById('toggleButton');
+    const halfWayPoint = document.documentElement.scrollHeight / 5;
+    const menu = document.getElementById('menu');
+
+    if (button) {
+        if (window.scrollY > halfWayPoint) {
+            button.classList.remove('hidden');
+        } else {
+            button.classList.add('hidden');
+        }
+    }
+
+    if (menu) {
+        if (window.scrollY <= halfWayPoint) {
+            menu.classList.remove('folded');
+        }
+    }
+});
+
+
+
+// Hide the menu above the top of the page when the button is clicked
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('toggleButton');
+    const menu = document.getElementById('menu');
+    if (button && menu) {
+        button.addEventListener('click', function() {
+            menu.classList.toggle('folded');
+        });
+    }
+});
