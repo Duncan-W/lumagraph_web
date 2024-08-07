eb27d151a1ab35b64a550b910aa3988e81217906
unknown <duncan.d.wallace@gmail.com>
2024-08-07 22:51:50 +0100

    minor

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,9 +9,7 @@
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
       <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
       
-                <p class="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">In hac habitasse platea
-                    dictumst
-                </p>
+               
             </div>
         </div>
         <div class="flex items-stretch justify-center">
