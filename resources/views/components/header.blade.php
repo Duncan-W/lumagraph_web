17e939c88187ceba9e33835106fa71adab78243c
unknown <duncan.d.wallace@gmail.com>
2024-08-07 11:46:26 +0100

    migration

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
     <header class="fixed w-full">
         <nav id="menu" class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
             <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                <a href="#" class="flex items-center">
+                <a href="./" class="flex items-center">
                     <img src="{{ asset('images/lumagraph.svg') }}" class="h-6 mr-3 sm:h-9" alt="Lumagraph Logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Lumagraph</span>
                 </a>
@@ -22,7 +22,7 @@
                 <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-800 rounded lg:bg-transparent lg:text-blue-800 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                            <a href="./" class="block py-2 pl-3 pr-4 text-white bg-blue-800 rounded lg:bg-transparent lg:text-blue-800 lg:p-0 dark:text-white" aria-current="page">Home</a>
                         </li>
                         <!-- <li>
                             <a href="#" class="block py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-800 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-800">Company</a>
