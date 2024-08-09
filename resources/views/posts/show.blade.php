6a9de436bff972327ecea2bd39ccbe2b1e1d51d6
unknown <duncan.d.wallace@gmail.com>
2024-08-09 17:37:53 +0100

    blog post styling

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,13 +10,15 @@
     </section>
     <div style="height: 20vh;"></div>
     <section class="bg-gray-50 dark:bg-gray-900">
-    <h1>{{ $post->title }}</h1>
-    @if ($post->image)
-        <img src="{{ $post->image }}" alt="{{ $post->title }}" style="max-width: 100%;">
-    @endif
-    <div class="prose">
-        {!! \Illuminate\Support\Str::markdown($post->body) !!}
-    </div>
-    <a href="{{ route('posts.index') }}">Back to Posts</a>
+        <div class="pt-8 px-4 mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">{{ $post->title }}</h1>
+            @if ($post->image)
+                <img src="{{ $post->image }}" alt="{{ $post->title }}" style="max-width: 100%;">
+            @endif
+        </div> 
+        <div class="prose" style="margin:auto">
+            {!! \Illuminate\Support\Str::markdown($post->body) !!}
+        </div>
+        <a href="{{ route('blog') }}" class="block ml-2 py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-800 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-800">Back to posts</a>
     </section>
 @endsection
