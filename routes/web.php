6da3cb211b8770d870dfefccf7a9dec1add13f4f
unknown <duncan.d.wallace@gmail.com>
2024-08-09 11:47:30 +0100

    route names

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,23 @@ Route::resource('posts', PostController::class);
 Route::get('/', function () {
     $latestPost = Post::latest()->last();
     return view('welcome', compact('latestPost'));
-});
+})->name('home');
 
 Route::get('/products', function () {
     return view('products');
-});
+})->name('products');
 
 Route::get('/team', function () {
     return view('team');
-});
+})->name('team');
 
 Route::get('/blog', function () {
     $posts = Post::all();
     $latestPost = Post::latest()->first();
     return view('blog', compact('posts'));
-});
+})->name('blog');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
