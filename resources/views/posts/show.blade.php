fd1e3d8f46ba68fbd92f03785241a361e0e4e6e0
unknown <duncan.d.wallace@gmail.com>
2024-11-21 13:08:07 +0000

    minor blog improvements

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,12 +26,11 @@
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="pt-8 px-4 mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">{{ $post->title }}</h1>
-                <img src="{{ $imageUrl }}" alt="{{ $post->title }}" style="max-width: 100%;">
+                <img src="{{ $imageUrl }}" alt="{{ $post->title }}" style="max-width: 100%;">\
         </div> 
-        <div class="prose" style="margin:auto">
+        <div class="prose sm:px-8 dark:text-white" style="margin:auto">
             {!! \Illuminate\Support\Str::markdown($post->body) !!}
         </div>
         <a href="{{ route('blog') }}" class="block ml-2 py-2 pl-3 pr-4 text-gray-800 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-800 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-800 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-800">Back to posts</a>
     </section>
-
 @endsection
